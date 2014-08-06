46076e91da427b2ac2bd508f7e9a367fea2669cb
Huntsss <hunter_robb@yahoo.com>
2014-08-06 17:36:13 -0500

    Finalized

--- noname.js
+++ noname.js
@@ -1,4 +1,4 @@
-var restify = require('restify');
+var restify = require('restify'),moment = require("moment");
 
 var server = restify.createServer({
   name: 'myapp',
@@ -12,13 +12,21 @@ server.use(restify.bodyParser());
 server.get('/echo/:name', function (req, res, next) {
 	var name = req.params.name;
 	console.log(name);
-  res.send("swag");
+	// inits moment module -> calls utc time -> get the value from the epoch -> casts it to string and sends it
+  res.send(moment().utc().valueOf().toString());
   res.end();
   return next();
 });
 
-server.post("/watever",function(req,res,next) {
-	var whatever = req.body.whatever;
+server.post("/post",function(req,res,next) {
+	// casts it to a JSON object from a string
+	var body = JSON.parse(req.body);
+	// json part .whatever from the json
+	var string = body.string;
+	console.log(string);
+  res.send("success");
+  res.end();
+  return next();
 });
 
 server.listen(8080, function () {
